34230070c9a64bbcac29761cbf31ba198e30cb6e
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-03-29 14:35:19 -0300

    Complementando a entidade Nutricionista.

--- src/main/java/br/com/zup/edu/nutricionistas/model/Nutricionista.java
+++ src/main/java/br/com/zup/edu/nutricionistas/model/Nutricionista.java
@@ -1,7 +1,47 @@
 package br.com.zup.edu.nutricionistas.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 public class Nutricionista {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String crn;
+
+    public Nutricionista(String nome, String cpf, LocalDate dataNascimento, String email, String crn) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.crn = crn;
+    }
+
+    @Deprecated
+    /**
+     * @deprecated Construtor para uso exclusivo do Hibernate.
+     */
+    public Nutricionista() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
 }
